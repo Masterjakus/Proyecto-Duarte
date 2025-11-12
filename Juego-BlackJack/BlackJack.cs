07b17930c5f9aaff083c16e0e0570b0d1cef72f3
Masterjakus <vlady_mar1603@hotmail.com>
2025-11-11 19:52:04 -0600

    habia folder vacios lmao

--- Juego-BlackJack/BlackJack.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Proyecto_Duarte.Juego_BlackJack;
-
-public class BlackJack
-{
-
-}
--- Juego-Uno/Uno.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Proyecto_Duarte.Juego_Uno;
-
-public class Uno
-{
-
-}
