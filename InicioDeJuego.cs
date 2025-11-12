d51aeeab319f6abc0fc1d2101b0713e97a1ab647
Masterjakus <vlady_mar1603@hotmail.com>
2025-11-11 19:33:55 -0600

    correcion inicio de juego

--- InicioDeJuego.cs
+++ InicioDeJuego.cs
@@ -1,28 +1,46 @@
 using System;
+using System.Buffers;
 
 namespace Proyecto_Duarte;
 
 public class InicioDeJuego
 {
     static void Main()
+    {
+        Console.WriteLine("Una apuestita?, en que quieres perder tu dinero?");
+        Console.WriteLine("1. UNO");
+        Console.WriteLine("2. BlackJack");
+        Console.WriteLine("Seleccciona uno (1 0 2):");
+
+        string opcion = Console.ReadLine();
+
+        JJuegoDeCartas juego = null;
+
+        if (opcion == "1")
         {
-            Console.WriteLine("Que queres jugar?");
-            Console.WriteLine("1. UNO");
-            Console.WriteLine("2. Blackjack");
-            Console.Write("👉 Opción: ");
-            if (!int.TryParse(Console.ReadLine(), out int opcion))
-                return;
-
-            JJuegoDeCartas juego = opcion switch
-            {
-                1 => new JuegoUNO.JuegoUNO(3),
-                2 => new JuegoBlackjack.JuegoBlackjack(3),
-                _ => throw new InvalidOperationException("Juego no válido")
-            };
-
-            juego.IniciarJuego();
-            juego.JugarRonda();
-            juego.MostrarGanador();
+            juego = new Juego_Uno.juegoUno(3);
         }
+        else if (opcion == "2")
+        {
+            juego = new Juego_BlackJack.JuegoBlackJack(3);
+        }
+        else
+        {
+            Console.WriteLine("eso no es valido pana, miedo?");
+            return;
+        }
+
+        Console.Clear();
+        Console.WriteLine("Bueno. que comience el juego!");
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"ronda {i}");
+            juego.JugarRonda();
+            Console.WriteLine(new string('-', 40));
+        }
+
+        Console.WriteLine("Bueno, el resultado es....");
+        juego.mostrarganador();
     }
 }
